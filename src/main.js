7e5ee1a5c5f41f177623b84f41bb5de7010a6dd8
simao234430 <simao234430@gmail.com>
2021-04-12 19:31:12 +0800

    添加基本语言设置

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import store from './store/'
 import router from './router'
 import App from './App.vue'
-
+import i18n from './locales'
+import './global.less' // global style
+// import 'ant-design-vue/dist/antd.less'
+import './core/use' // use lazy load components
+import './core/lazy_use' // use lazy load components
 Vue.config.productionTip = false
 
 new Vue({
   store,
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack')
+const buildDate = JSON.stringify(new Date().toLocaleString())
+// check Git
+function getGitHash () {
+  try {
+    return GitRevision.version()
+  } catch (e) {}
+  return 'unknown'
+}
+
+const isProd = process.env.NODE_ENV === 'production'
+// vue.config.js
+const vueConfig = {
+  configureWebpack: {
+    // webpack plugins
+    plugins: [
+      // Ignore all locale files of moment.js
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new webpack.DefinePlugin({
+        APP_VERSION: `"${require('./package.json').version}"`,
+        GIT_HASH: JSON.stringify(getGitHash()),
+        BUILD_DATE: buildDate
+      })
+    ],
+    // if prod, add externals
+    externals: isProd ? assetsCDN.externals : {}
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          // less vars，customize ant design theme
+
+          // 'primary-color': '#F5222D',
+          // 'link-color': '#F5222D',
+          'border-radius-base': '2px'
+        },
+        // DO NOT REMOVE THIS LINE
+        javascriptEnabled: true
+      }
+    }
+  },
+}
+
+module.exports = vueConfig
